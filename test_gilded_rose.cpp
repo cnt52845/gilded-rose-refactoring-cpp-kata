9e12a71ad4d4f48712a3a479b593057bf314f934
skoterev <skoterev@ford.com>
2025-04-23 11:48:53 -0400

    Implement test_conjured_item_quality_is_never_negative

--- test_gilded_rose.cpp
+++ test_gilded_rose.cpp
@@ -113,3 +113,11 @@ TEST_F(GildedRoseTest, test_conjured_item_quality_decreases_by_four_after_sell_i
 
     EXPECT_EQ(inn.items.at(0)->quality, 1);
 }
+
+TEST_F(GildedRoseTest, test_conjured_item_quality_is_never_negative)
+{
+    inn.add_item("Conjured", 0, 2);
+    inn.update_quality();
+
+    EXPECT_EQ(inn.items.at(0)->quality, 0);
+}
